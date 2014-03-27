c3c53e37fc9c9416ba89ed508eedd89def6064b0
Pito Salas <pitosalas@gmail.com>
2014-03-27 17:45:17 -0400

    demo

--- demo3.js
+++ demo3.js
@@ -8,6 +8,9 @@
 //	Notice muliple assignments
 
 var x = 100;
+x = "a string";
+x = true;
+
 
 var name = "Pito", town = "Arlington";
 
@@ -18,16 +21,21 @@ var iAmUndefined;
 var no1 = 100;
 
 var no2 = 200;
+var no3 = 100
 
 if (no1 == 100) {
-	document.write("100 == 100")
+	document.write("<p>100 == 100</p>");
 }
 if (no1 === 100) {
-	document.write("100 === 100")
+	document.write("<p>100 === 100</p>");
 }
 if (no1 == "100") {
-	document.write("100 == '100'")
+	document.write("<p>100 == '100'</p>");
 }
 if (no1 === "100") {
-	document.write("100 === '100'")
+	document.write("<p>100 === '100'</p>");
 }
+if (no1 === no3) {
+	document.write("<p>100 === no3</p>");
+}
+
--- demo5.js
+++ demo5.js
@@ -2,15 +2,21 @@
 // 	Notice that getElementsByClassName returns an array!
 // 	Also notice that the javacript script tag comes after all the html!
 
-console.log(document.getElementsByClassName('author')[0].innerHTML);
-console.log(document.getElementById('first').innerHTML);
+//console.log(document.getElementsByClassName('author')[0].innerHTML);
+
+//console.log(document.getElementById('first').innerHTML);
 
 // Now lets try to actually change the html file!
 
 // Change the author's html
 document.getElementsByClassName('author')[0].innerHTML = "Pito Salas";
+
 document.getElementsByClassName('author')[0].style.color = "red";
+
 document.getElementsByClassName('author')[0].style.fontStyle = "italic";
+
 document.getElementsByClassName('author')[0].style.fontSize = "200%";
+
+console.log(document.getElementsByClassName('stanza').length);
 
 
